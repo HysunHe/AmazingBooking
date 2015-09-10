4e84c42d887a8998c3ec2d66da99ae53fba0a48e
HysunHe <Hysun.He@AsiaInspection.com>
2015-09-10 12:40:00 +0800

    Created utils package

---
+++ src/main/java/com/ai/hackathon/amazingbooking/utils/MailParser.java
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *
+ * This document contains confidential and proprietary information 
+ * subject to non-disclosure agreements with AsiaInspection. This 
+ * information shall not be distributed or copied without written 
+ * permission from the AsiaInspection.
+ *
+ ***************************************************************************/
+
+package com.ai.hackathon.amazingbooking.utils;
+
+/***************************************************************************
+ * <PRE>
+ *  Project Name    : AmazingBooking
+ * 
+ *  Package Name    : com.ai.hackathon.amazingbooking.utils
+ * 
+ *  File Name       : MailParser.java
+ * 
+ *  Creation Date   : 2015年9月10日
+ * 
+ *  Author          : Hysun He
+ * 
+ *  Purpose         : TODO
+ * 
+ * 
+ *  History         : TODO
+ * 
+ * </PRE>
+ ***************************************************************************/
+public final class MailParser {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
