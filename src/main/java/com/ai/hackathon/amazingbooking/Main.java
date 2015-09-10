07a865ec4f59db5897262b554d3811a01d7e0a6b
HysunHe <Hysun.He@AsiaInspection.com>
2015-09-10 12:37:14 +0800

    Create main class

---
+++ src/main/java/com/ai/hackathon/amazingbooking/Main.java
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *
+ * This document contains confidential and proprietary information 
+ * subject to non-disclosure agreements with AsiaInspection. This 
+ * information shall not be distributed or copied without written 
+ * permission from the AsiaInspection.
+ *
+ ***************************************************************************/
+
+package com.ai.hackathon.amazingbooking;
+
+/***************************************************************************
+ * <PRE>
+ *  Project Name    : AmazingBooking
+ * 
+ *  Package Name    : com.ai.hackathon.amazingbooking
+ * 
+ *  File Name       : Main.java
+ * 
+ *  Creation Date   : 2015年9月10日
+ * 
+ *  Author          : Hysun He
+ * 
+ *  Purpose         : TODO
+ * 
+ * 
+ *  History         : TODO
+ * 
+ * </PRE>
+ ***************************************************************************/
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
--- src/main/java/com/ai/hackathon/amazingbooking/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 
- */
-package com.ai.hackathon.amazingbooking;
