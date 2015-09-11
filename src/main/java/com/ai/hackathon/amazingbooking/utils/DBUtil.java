c9817976a8d527cfbcb49c41da718f8cffbaa3f8
HysunHe <Hysun.He@AsiaInspection.com>
2015-09-11 10:13:09 +0800

    Load jdbc driver only once.

--- src/main/java/com/ai/hackathon/amazingbooking/utils/DBUtil.java
+++ src/main/java/com/ai/hackathon/amazingbooking/utils/DBUtil.java
@@ -1,106 +1,78 @@
 package com.ai.hackathon.amazingbooking.utils;
 
-import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.sql.DataSource;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 public final class DBUtil {
-
-	private static Log log = LogFactory.getLog(DBUtil.class.getName());
-
-	private static ComboPooledDataSource dataSource;
-
 	private static final String dbDriver = "oracle.jdbc.driver.OracleDriver";
 	private static final String dbUrl = "jdbc:oracle:thin:@192.168.0.6:1521:aidev";
 	private static final String dbUser = "aidatauser";
 	private static final String dbPass = "tiger";
 
+	static {
+		try {
+			Class.forName(dbDriver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("!!! JDBC driver not found!");
+			System.exit(1);
+		}
+	}
+
 	private DBUtil() {
 	}
 
+	/**
+	 * @return
+	 */
 	public static Connection getConnection() {
 		Connection conn = null;
-
 		try {
-			Class.forName(dbDriver);
 			conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
-
-			log.debug("connection:" + conn);
+			System.out.println("connection:" + conn);
 		} catch (Exception e) {
-			log.error("Error:", e);
+			e.printStackTrace();
 		}
-
 		return conn;
 	}
 
-	public static DataSource getDataSource() {
-		if (dataSource == null) {
-
-			dataSource = new ComboPooledDataSource();
-			try {
-				dataSource.setDriverClass(dbDriver);
-			} catch (PropertyVetoException e) {
-				log.error("Error: ", e);
-			}
-			dataSource.setJdbcUrl(dbUrl);
-			dataSource.setUser(dbUser);
-			dataSource.setPassword(dbPass);
-
-			dataSource.setInitialPoolSize(1);
-			dataSource.setMinPoolSize(1);
-			dataSource.setMaxPoolSize(500);
-			dataSource.setAcquireIncrement(0);
-			dataSource.setMaxIdleTime(300);
-
-			// close Statement, PreparedStatement cache
-			dataSource.setMaxStatements(0);
-			dataSource.setMaxStatementsPerConnection(0);
-
-			dataSource.setIdleConnectionTestPeriod(100);
-		}
-
-		return dataSource;
-	}
-
+	/**
+	 * @param conn
+	 */
 	public static void close(Connection conn) {
 		try {
 			if (conn != null) {
 				conn.close();
 			}
 		} catch (SQLException e) {
-			log.error("Error:", e);
+			e.printStackTrace();
 		} finally {
 			try {
 				conn.close();
 			} catch (SQLException e) {
-				log.error("Error:", e);
+				e.printStackTrace();
 			}
 		}
 	}
 
+	/**
+	 * @param stat
+	 */
 	public static void close(Statement stat) {
 		try {
 			if (stat != null) {
 				stat.close();
 			}
 		} catch (SQLException e) {
-			log.error("Error:", e);
+			e.printStackTrace();
 		} finally {
 			try {
 				stat.close();
 			} catch (SQLException e) {
-				log.error("Error:", e);
+				e.printStackTrace();
 			}
 		}
 	}
-
 }
